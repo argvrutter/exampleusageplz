d0f98db6377a20f5a174e6fa8b34dce2784ab28d
jlangst6 <jlangst6@vols.utk.edu>
2022-03-18 21:45:01 -0400

    added sqlalchemy stuff

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -16,8 +16,18 @@ app.config['SWAGGER'] = {
 swagger = Swagger(app)
 
 ## Initialize Config
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('../config.py')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #register blueprints
 app.register_blueprint(home_api, url_prefix='/api')
+
+db = SQLAlchemy()
+db.init_app(app)
+ 
+@app.before_first_request
+def create_table():
+    db.create_all()
 
