7dee2786c49537f45e8065f77f6842c0d5303b79
argvrutter <solsane@gmail.com>
2022-04-11 23:04:38 -0400

    Add simple API use cases, (create and read posts for one call)

--- frontend/exampleusageplz/src/server.ts
+++ frontend/exampleusageplz/src/server.ts
@@ -6,6 +6,7 @@
 import axios from 'axios';
 // import { AxiosResponse } from 'axios';
 // import URI class
+import {Post, Call, Api} from './interface';
 
 /** 
  * 
@@ -18,7 +19,6 @@ import axios from 'axios';
 * PUT: /api/apis/</id/> - updates an api
 * DELETE: /api/apis/</id/> - deletes an api
 */
-
 /**
  * This will primarily use the search endpoint to get a list of posts matching a query
  * The query includes the package name, a list of calls
@@ -39,6 +39,24 @@ export class Server {
     constructor(token : string, baseUrl : string) {
         this.token = token;
         this.baseUrl = baseUrl;
+    }
+
+    // get a list of posts matching a Call
+    public async getPostsByCall(call : Call) : Promise<Post[]> {
+        // get the list of posts
+        let response = await axios.get(this.baseUrl + '/api/posts/search/' + call.name);
+        // return the list of posts
+        // this assumes that it has a list of posts
+        // TODO: this will be paginated at some point
+        return response.data.posts;
+    }
+
+    // submit a new post
+    public async submitPost(post : Post) : Promise<Post> {
+        // create the post
+        let response = await axios.post(this.baseUrl + '/api/posts', post);
+        // return the post
+        return response.data;
     }
 
 }
