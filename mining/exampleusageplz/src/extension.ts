913e71f01ab81750e04f78f27a633327f0900f97
argvrutter <solsane@gmail.com>
2022-04-27 21:00:19 -0400

    do the thing

--- mining/exampleusageplz/src/extension.ts
+++ mining/exampleusageplz/src/extension.ts
@@ -6,6 +6,7 @@ import { showQuickPick } from './post';
 import { VirtualDocProvider } from './document_provider';
 import { Post, usageInstanceToCall } from './interface';
 import Client from './client';
+import console = require('console');
 
 export async function activate(context: vscode.ExtensionContext) {
 	vscode.window.onDidChangeActiveTextEditor(() => {
@@ -27,7 +28,7 @@ export async function activate(context: vscode.ExtensionContext) {
 		"exampleusageplz",
 		virtDocProvider
 	);
-	
+
 	let openCodelensDisposable = vscode.commands.registerCommand("exampleusageplz.getUsage", async () => {
 		vscode.workspace.getConfiguration("exampleusageplz").update("exampleUsageCodeLens", true);
 	});
@@ -36,9 +37,9 @@ export async function activate(context: vscode.ExtensionContext) {
 		vscode.workspace.getConfiguration("exampleusageplz").update("exampleUsageCodeLens", false);
 	});
 
-    let codelensDisposable = vscode.commands.registerCommand("exampleusageplz.addUsageInfo", (args: any) => {
+	let codelensDisposable = vscode.commands.registerCommand("exampleusageplz.addUsageInfo", (args: any) => {
 		openPeekView(args);
-    });
+	});
 
 	// Add a command for quickly submitting a post
 	// based on quickinput-sample: 
@@ -51,38 +52,54 @@ export async function activate(context: vscode.ExtensionContext) {
 	// get the name of the current workspace
 	const workspaceName = vscode.workspace.name;
 
-	// get all typescript files in the workspace
-	let allFiles = await vscode.workspace.findFiles('**/*.ts');
-	
+	// get all typescript files in the workspace not in the node_modules or dist folder
+
+	const allFiles = await vscode.workspace.findFiles('**/*.ts', '**/node_modules/**');
+	// filter out dist
+	const files = allFiles.filter(f => !f.path.includes('dist'));
 	const baseUrl = 'http://localhost:5000';
-	// open them all
-	for (let file of allFiles) {
-		vscode.window.showInformationMessage(`Opening ${file.path}`);
-		vscode.workspace.openTextDocument(file.path).then(
-			(document) => {
-				codeLensProvider.startCodelens();
+	const server = new Client(baseUrl);
+	// open them all using map
+	const openFiles = files.map(async (f) => {
+		// vscode.window.showInformationMessage(`Opening ${file.path}`);
+		console.log("opening file: ", f.path);
+		await vscode.workspace.openTextDocument(f.path).then(
+			async (document) => {
+				await codeLensProvider.startCodelens();
 				// get function calls
-				codeLensProvider.funcList.forEach(async (func) => {
+				const filemap = codeLensProvider.funcList.map(async (func) => {
 					// get the function name
 					const title = workspaceName + ": " + func._name;
 					// get the text of the document 10 lines before and after the function call
-					const text = await document.getText(new vscode.Range(func._position.start.line - 10, 0, func._position.end.line + 10, 0));
+					// construct range, accounting for beginning and end of document
+					const range = new vscode.Range(
+						new vscode.Position(Math.max(0, func._position.start.line - 10), 0),
+						new vscode.Position(Math.min(document.lineCount - 1, func._position.end.line + 10), 0)
+					);
+							
+					const text = await document.getText(range);
 					const post = {
 						title: title,
 						content: text,
 						call: usageInstanceToCall(func, document.languageId)
 					};
 					// post the post
-					
-					const server = new Client(baseUrl);
+
 					const newPost = await server.submitPost(post);
+					console.log("new post: ", newPost);
 				});
-			});
-	}
+				await Promise.all(filemap);
+			}
+		);
+	});
 
 	context.subscriptions.push(codeLensProviderDisposable, openCodelensDisposable,
-								codelensDisposable, quickPostDisposable, virtualDocContentProvider);
-
+		codelensDisposable, quickPostDisposable, virtualDocContentProvider);
+	
+	await Promise.all(openFiles);
+	console.log("ExampleUsage extension is now active!");
+	// exit after done
+	vscode.commands.executeCommand("workbench.action.closeWindow");
 }
 
-export function deactivate() {}
+export function deactivate() { }
